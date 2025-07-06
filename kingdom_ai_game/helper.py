a66c637c76faab12f817c39928a81bd421e7626e
PACWM <matteo-fontana@hotmail.com>
2025-07-06 18:14:46 +0200

    env_setup

--- kingdom_ai_game/helper.py
+++ kingdom_ai_game/helper.py
@@ -2,6 +2,7 @@
 
 import os
 from dotenv import load_dotenv, find_dotenv
+import json
 
 # these expect to find a .env file at the directory above the lesson.                                                         # the format for that file is (without the comment)                                                                           # API_KEYNAME=AStringThatIsTheLongAPIKeyFromSomeService                                                                                                                                     
 def load_env():
